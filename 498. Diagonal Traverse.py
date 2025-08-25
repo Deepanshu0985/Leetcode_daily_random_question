8bb23bac8e2263640d215e7ce69e04f81b8e704a
Deepanshu0985 <deepanshuy098@gmail.com>
2025-08-25 16:32:02 +0530

    final

--- 498. Diagonal Traverse.py
+++ 498. Diagonal Traverse.py
@@ -32,6 +32,6 @@ def findDiagonalOrder(mat):
                 col-=1
     return ans
 
-print(findDiagonalOrder([[1,2,3],[4,5,6],[7,8,9]]))
+print(findDiagonalOrder([[1,2,3],[4,5,6],[7,8,9],[10,11,12,13]]))
                 
                 
