8c78ec3411f884a8d0f0b31484c7059f3f9c25f0
Deepanshu0985 <deepanshuy098@gmail.com>
2025-08-25 14:03:44 +0530

    final

---
+++ 498. Diagonal Traverse.py
@@ -0,0 +1,37 @@
+
+from traitlets import List
+def findDiagonalOrder(mat):
+    n= len(mat)
+    m = len(mat[0])
+    row = 0
+    col =0
+    up = True
+    ans =[]
+    
+    for _ in range(n*m):
+        ans.append(mat[row][col])
+        if up:
+            if col == m-1:
+                row+=1
+                up = not up
+            elif row == 0:
+                col+=1
+                up = not up
+            else:
+                row-=1
+                col+=1
+        else:
+            if row==n-1:
+                col+=1
+                up = not up
+            elif col == 0:
+                row+=1
+                up = not up
+            else:
+                row+=1
+                col-=1
+    return ans
+
+print(findDiagonalOrder([[1,2,3],[4,5,6],[7,8,9]]))
+                
+                
